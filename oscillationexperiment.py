9aeb11bf0027ef90794431089f47af7e6f040bde
Parva-Alavian <parva.alavian@gmail.com>
2025-04-28 13:19:40 +0200

    fix one parameter value mistake

--- oscillationexperiment.py
+++ oscillationexperiment.py
@@ -150,7 +150,7 @@ def main():
     noise_off = {"exc1.sigma":0.0, "exc2.sigma":0.0, "pv.sigma":0.0, "sst1.sigma":0.0, "sst2.sigma":0.0, "vip1.sigma":0.0, "vip2.sigma":0.0}
     disconnect_sst_to_vip = {"J.vip1.sst1": 0.0, "J.vip2.sst2": 0.0}
     disconnect_E_to_sst_vip = {"J.sst1.exc1":0.0, "J.sst2.exc2":0.0, "J.vip1.exc1":0.0, "J.vip2.exc2":0.0, "J_ampa.sst1.exc1":0.0, "J_ampa.sst2.exc2":0.0, "J_ampa.vip1.exc1":0.0, "J_ampa.vip2.exc2":0.0}
-    disconnect_sst_to_E = {"J.exc1.sst1":0.0, "J.exc.sst2":0.0}
+    disconnect_sst_to_E = {"J.exc1.sst1":0.0, "J.exc2.sst2":0.0}
     circuit_params = noise_off | disconnect_E_to_sst_vip | disconnect_sst_to_E
     params.batch_update(circuit_params)
     params.save(folder/'params.json')
